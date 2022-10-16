851a04739e87cee9afeec8ef1a8c51448c5eaacd
Anisa Yousuf <anisayousuf@hotmail.co.uk>
2022-10-16 12:43:41 +0100

    Added more detailed print statements

--- homework-1/cash_register.py
+++ homework-1/cash_register.py
@@ -24,47 +24,47 @@ class CashRegister:
         self.total_price = 0
         self.discount = 0
 
-    def add_item(self, item, price):             # add a new item to the items we already have (update method)
+    def add_item(self, item, price):
         self.total_items.update({item: price})
-        print(item, price)
+        print(f"The item {item} has been added.")
 
-    def remove_item(self, item):                 # take an item away from the items we already have (pop method)
+    def remove_item(self, item):
         self.total_items.pop(item)
-        print(self.total_items)
+        print(f"The item {item} has been removed. \nItems remaining: {self.total_items}")
 
-    def apply_discount(self, discount):          # apply unknown discount amount
+    def apply_discount(self, discount):
         self.discount = discount
         discount_decimal = discount / 100
         discounted_total = sum(self.total_items.values()) * (1 - discount_decimal)
-        print(discounted_total)                  # need it to be to 2dp
+        print(f"{discount}% discount applied. \nNew total price: £{discounted_total:.2f}")
 
-    def get_total(self):                         # all the item prices added up (sum method)
+    def get_total(self):
         self.total_price = sum(self.total_items.values())
-        print(self.total_price)
+        print(f"Total Price: £{self.total_price:.2f}\n")
 
-    def show_items(self):                        # return all the items currently in the dictionary total_items
-        print(self.total_items)
+    def show_items(self):
+        print(f"Current items: {self.total_items}")
 
-    def reset_register(self):                    # remove everything from total_items and set it all back to zero
+    def reset_register(self):
         self.total_items.clear()
         self.total_price = 0
         self.discount = 0
+        print("Cash register has been reset")
 
 
 # EXAMPLE code run:
 customer = CashRegister()
-customer.add_item("Bag", 2.50)
-customer.add_item("Jumper", 8.99)
-customer.add_item("Socks", 3.00)
+
+customer.add_item("Bag", 2.75)
+customer.add_item("Jumper", 11.99)
+customer.add_item("Socks", 3.50)
 
 customer.remove_item("Bag")
 
-customer.apply_discount(15)
+customer.apply_discount(3.25)
 
 customer.get_total()
 
 customer.show_items()
 
 customer.reset_register()
-
-
