b7bfdcfbc5c815c126bb744851ae14b7f24ab364
Anisa Yousuf <anisayousuf@hotmail.co.uk>
2022-11-01 14:33:17 +0000

    Added my solution

--- homework-4/search_in_matrix.py
+++ homework-4/search_in_matrix.py
@@ -29,4 +29,26 @@ result = [3,3]
 
 
 def search_in_matrix(matrix, target):
-    pass
+    rows = len(matrix)
+    columns = len(matrix[0])
+
+    for i in range(rows):
+        for j in range(columns):
+            if target == matrix[i][j]:
+                return [i, j]
+    return [-1, -1]
+
+
+matrix = [
+[1,4,7,12,15,1000],
+[2,5,19,31,32,1001],
+[3,8,24,33,35,1002],
+[40,41,42,44,45,1003],
+[99,100,103,106,128,1004]
+]
+
+
+# Test Cases:
+print(search_in_matrix(matrix=matrix, target=7))
+print(search_in_matrix(matrix=matrix, target=103))
+print(search_in_matrix(matrix=matrix, target=1010))
